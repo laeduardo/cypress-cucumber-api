8917e27eb827ebcdc127c3587cf59d9c8af5bc5e
Eduardo Lopes Amaro <dudulamaro@gmail.com>
2024-09-24 20:53:18 -0300

    CI workflow

--- cypress.config.js
+++ cypress.config.js
@@ -17,6 +17,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
     },
-    specPattern: ["cypress/e2e/step_definitions/*.feature"]
+    specPattern: ["cypress/e2e/step_definitions/*.feature", "cypress/api/*.js"]
   },
 });
--- cypress/api/dashboard_api.js
+++ cypress/api/dashboard_api.js
@@ -1,30 +1,14 @@
-import LoginPage from "../support/pageobjects/loginPage"
+import LoginPage from "../support/page_objects/login_page"
 const loginPage = new LoginPage
 
-context('Testar API do Finance', () => {
+context('Testar API', () => {
 
-    before(() => {
-        loginPage.acessarLogin()
-        const email = Cypress.env('USERNAME')
-        const senha = Cypress.env('PASSWORD')
-        loginPage.fazerLogin(email, senha)
-    })
-
-    it('GET - Supplier', () => {
-        cy.get('@bearerToken')
-          .then((token) => {
-            const bearerToken = token
-
-            cy.api({
+    it('GET - UNIT', () => {
+        cy.api({
                 method: 'GET',
-                url: 'https://mqm76sseb6.execute-api.eu-west-1.amazonaws.com/v1/suppliers?search=9',
-                headers: {
-                    Authorization: `Bearer ${bearerToken}`,
-                    Origin: "https://qa1-finance.clevercorporate.com"
-                }
+                url: '/',
             }).then((response) => {
                 expect(response.status).to.eq(200)
             })
         })
     })
-})
